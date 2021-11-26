0c3d2164865d09c38413914889d516f340ad3de7
Pubet Choompusueb <pubet50@gmail.com>
2021-11-26 16:59:35 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    int even=-1;
+    int odd=0;
+    int i;
+    while(i!=0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i%2==0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
     cout << "#Even numbers = ";
+    cout << even << "\n";
     cout << "#Odd numbers = ";
+    cout << odd;
+
+
+
+
+
+
     return 0;
 }
